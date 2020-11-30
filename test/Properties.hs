e2d7a3ca3f64ad1984d59f05eb65188d2e809230
chessai <chessai1996@gmail.com>
2020-11-30 15:15:54 -0800

    add more property tests

--- test/Properties.hs
+++ test/Properties.hs
@@ -1,3 +1,4 @@
+{-# language LambdaCase #-}
 {-# language ScopedTypeVariables #-}
 {-# language TypeApplications #-}
 
@@ -5,21 +6,63 @@ module Main (main) where
 
 import Control.Monad (forM_)
 import Control.Monad.IO.Class (liftIO)
+import Data.Monoid (Last(..))
+import GHC.Exts (fromList)
+import Prelude hiding (foldMap)
 import Test.QuickCheck
 import Test.QuickCheck.Monadic
-import RingBuffers.Lifted as R
+import qualified Data.List as List
+import qualified RingBuffers.Lifted as R
 
 main :: IO ()
 main = do
-  quickCheck (testAppend @Int)
+  quickCheck $ append @Int
+  quickCheck $ newIsEmpty
+  quickCheck $ latest @Int
+  quickCheck $ foldMap @Int
 
-testAppend :: forall a. (Eq a, Show a)
+append :: forall a. (Eq a, Show a)
   => Positive Int
   -> [a]
   -> Property
-testAppend (Positive cap) xs = monadicIO $ do
+append (Positive cap) xs = monadicIO $ do
   xs' <- liftIO $ do
     rb <- R.new @a cap
     forM_ (reverse xs) $ \x -> R.append x rb
     R.toList rb
   pure $ counterexample (show xs') $ xs' == take cap xs
+
+newIsEmpty :: ()
+  => Positive Int
+  -> Property
+newIsEmpty (Positive cap) = monadicIO $ do
+  rb <- liftIO $ R.new @Int cap
+  rbCap <- liftIO $ R.capacity rb
+  rbLen <- liftIO $ R.filledLength rb
+  pure $ rbCap == cap && rbLen == 0
+
+latest :: forall a. (Eq a, Show a)
+  => Positive Int
+  -> [a]
+  -> Property
+latest (Positive cap) xs = monadicIO $ do
+  rb <- liftIO $ R.new @a cap
+  let go = \case
+        [] -> do
+          pure ()
+        (a : as) -> do
+          liftIO $ R.append a rb
+          l <- liftIO $ R.latest rb 0
+          assert (Just a == l)
+          go as
+  go xs
+
+foldMap :: forall a. (Ord a, Show a)
+  => Positive Int
+  -> [a]
+  -> Property
+foldMap (Positive cap) xs = monadicIO $ do
+  rb <- liftIO $ R.new @a cap
+  liftIO $ R.extend (fromList xs) rb
+  m <- liftIO $ R.foldMap rb $ \x -> pure [x]
+  pure $ counterexample (show m) $ all (`elem` xs) m
